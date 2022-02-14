b538dc0c0a7c9779b6a33af74165334bef0313e7
pestirA <pauline@techieunicorn.com>
2022-02-14 11:57:53 -0500

    init

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ terraform {
     hostname     = "app.terraform.io"
     token        = "cxo3MxJ1k0n7dA.atlasv1.ILshRz44PouoMWneKm8fMLFDiuQN5UuzhXbAkaMhr0GTS4YHOvb9tHxa3ccAeEuZyZ8"
     workspaces {
-      name = "tfc-workspace-gcp-projg"
+      name = "tfc-workspace-gcp-np"
     }
   }
 }
